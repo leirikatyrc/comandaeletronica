603ac8f52d7b79ba3ae6907da8191f6114b97d95
leirikatrc <leirikatytj@hotmail.com>
2016-09-07 18:38:51 -0300

    ajuste AbstractTest

--- src/test/java/br/com/test/comandaeletronica/utils/AbstractTest.java
+++ src/test/java/br/com/test/comandaeletronica/utils/AbstractTest.java
@@ -2,11 +2,10 @@ package br.com.test.comandaeletronica.utils;
 
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.SpringApplicationConfiguration;
-import org.springframework.boot.test.util.ApplicationContextTestUtils;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 @RunWith(SpringJUnit4ClassRunner.class)
-@SpringApplicationConfiguration(classes = AbstractTest.class)
+@SpringApplicationConfiguration(classes = AppContextTest.class)
 public abstract class AbstractTest {
 
 }
