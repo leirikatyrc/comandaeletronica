a666773fb56f6d26ca31e866d8101f5755168899
leirikatrc <leirikatytj@hotmail.com>
2016-09-07 15:57:20 -0300

    Projeto Inicial

---
+++ src/main/java/br/com/comandaeletronica/ComandaeletronicaApplication.java
@@ -0,0 +1,12 @@
+package br.com.comandaeletronica;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ComandaeletronicaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ComandaeletronicaApplication.class, args);
+	}
+}
